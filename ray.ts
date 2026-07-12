154280ab04898bb994702046a8e68f0a2deb150d
Vinicius <viniciusgoncalves2000@hotmail.com>
2026-07-12 13:34:39 -0300

    adds ray class

--- ray.ts
+++ ray.ts
@@ -1,16 +1,29 @@
 import * as THREE from 'three';
 
 export class Ray {
-    private readonly origin: THREE.Vector3;
-    private readonly direction: THREE.Vector3;
+    public readonly origin: THREE.Vector3;
+    public readonly direction: THREE.Vector3;
 
-    public constructor(origin: THREE.Vector3, direction: THREE.Vector3
+    public constructor(origin?: THREE.Vector3, direction?: THREE.Vector3
     ) {
-        this.origin = origin.clone();
-        this.direction = direction.clone().normalize();
+        this.origin = origin?.clone() ?? new THREE.Vector3();
+        this.direction = direction?.clone().normalize() ?? new THREE.Vector3();
     }
 
     public at(t: number): THREE.Vector3 {
         return this.origin.clone().add(this.direction.clone().multiplyScalar(t));
+    }
+
+    public set(origin: THREE.Vector3, direction: THREE.Vector3): void {
+        this.origin.copy(origin);
+        this.direction.copy(direction).normalize();
+    }
+
+    public setOrigin(origin: THREE.Vector3): void {
+        this.origin.copy(origin);
+    }
+
+    public setDirection(direction: THREE.Vector3): void {
+        this.direction.copy(direction).normalize();
     }
 }
